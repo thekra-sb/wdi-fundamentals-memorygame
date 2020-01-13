6da677beae44243106e8e8441f27585b0deb29fa
Thekra Bawazeer <thekrabawazeer@Thekras-MacBook-Pro.local>
2020-01-13 04:26:09 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+
+let cardOne = cards[0];
 console.log("User flipped " + cardOne);
+
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[1];
 console.log("User flipped " + cardTwo);
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
