918d4d9be2a94d16a5a1feff3c4b89b23693d105
Thekra Bawazeer <thekrabawazeer@Thekras-MacBook-Pro.local>
2020-01-14 05:30:43 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,53 @@
 console.log("Up and running!");
 
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+	];
 let cardsInPlay = [];
 
-function flipCard(cardId){
-
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again.");
 		}
+}
+
+function flipCard(cardId){
+
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
 }
 
 flipCard(0);
 flipCard(2);
+
+console.log(cards[0].suit);
+console.log(cards[0].cardImage);
+
+console.log(cards[2].suit);
+console.log(cards[2].cardImage);
