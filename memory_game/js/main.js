025091c38de2839aed28eba2558dfa4afa76e00d
Thekra Bawazeer <thekrabawazeer@Thekras-MacBook-Pro.local>
2020-01-15 20:26:48 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,26 +28,58 @@ let cardsInPlay = [];
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
+			score();
 		} else {
 			alert("Sorry, try again.");
 		}
+
+	cardsInPlay.pop();
+	cardsInPlay.pop();	
 }
 
-function flipCard(cardId){
+function flipCard(){
 
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 
-	if (cardsInPlay.length === 2) {
+	if (cardsInPlay.length % 2 === 0) {
 		checkForMatch();
 	}
 }
-
-flipCard(0);
-flipCard(2);
 
 console.log(cards[0].suit);
 console.log(cards[0].cardImage);
 
 console.log(cards[2].suit);
 console.log(cards[2].cardImage);
+
+function createBoard(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
+createBoard();
+
+function resetfun(){
+	//location.reload();
+	createBoard();
+}
+
+function score() {
+	var score = document.getElementsByTagName('td')[1].textContent;
+	score = parseInt(score);
+	console.log("current score = " + score);
+	score += 1;
+	document.getElementsByTagName('td')[1].innerHTML = score;
+	console.log(" score after addition = " + score);
+
+}
